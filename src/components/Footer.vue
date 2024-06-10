747399e72871ed18acaee4b24dec1ba4ecec6ab9
Blaise-dev <blaiseatangana5@gmail.com>
2024-06-10 21:23:27 +0200

    Fin de l'implémentation de l'éntête.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@ import Logo from '../components/Logo.vue'
 </script>
 
 <template>
-  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top m-5">
     <div class="col mb-3"></div>
     <div class="col mb-3">
       <Logo class="d-block w-25 h-25" />
@@ -20,13 +20,15 @@ import Logo from '../components/Logo.vue'
           <RouterLink to="/" class="nav-link p-0 text-body-secondary">Boutique</RouterLink>
         </li>
         <li class="nav-item mb-2">
-          <RouterLink to="#" class="nav-link p-0 text-body-secondary">Qui sommes nous</RouterLink>
+          <RouterLink to="/about" class="nav-link p-0 text-body-secondary"
+            >Qui sommes nous</RouterLink
+          >
         </li>
         <li class="nav-item mb-2">
-          <RouterLink to="#" class="nav-link p-0 text-body-secondary">FAQs</RouterLink>
+          <RouterLink to="/faq" class="nav-link p-0 text-body-secondary">FAQs</RouterLink>
         </li>
         <li class="nav-item mb-2">
-          <RouterLink to="#" class="nav-link p-0 text-body-secondary">Contact</RouterLink>
+          <RouterLink to="/contact" class="nav-link p-0 text-body-secondary">Contact</RouterLink>
         </li>
       </ul>
     </div>
@@ -35,13 +37,17 @@ import Logo from '../components/Logo.vue'
       <h5>Mon Compte</h5>
       <ul class="nav flex-column">
         <li class="nav-item mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">Mon Compte</a>
+          <RouterLink to="/account" class="nav-link p-0 text-body-secondary"
+            >Mes paramètres</RouterLink
+          >
         </li>
         <li class="nav-item mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">Vos souhaits</a>
+          <RouterLink to="/wishlist" class="nav-link p-0 text-body-secondary"
+            >Vos souhaits</RouterLink
+          >
         </li>
         <li class="nav-item mb-2">
-          <a href="#" class="nav-link p-0 text-body-secondary">Commande</a>
+          <RouterLink to="/orders" class="nav-link p-0 text-body-secondary">Commande</RouterLink>
         </li>
       </ul>
     </div>
