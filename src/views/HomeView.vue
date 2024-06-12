bfe9f446a336e21d546bcfcaa909f3a87c317527
Blaise-dev <blaiseatangana5@gmail.com>
2024-06-12 18:32:17 +0200

    Correction de bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,19 +30,28 @@
         bottomText="Bottom Text for Slide 3"
       />
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleSlidesOnly" role="button" data-slide="prev">
+    <a
+      class="carousel-control-prev"
+      href="#carouselExampleSlidesOnly"
+      role="button"
+      data-slide="prev"
+    >
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleSlidesOnly" role="button" data-slide="next">
+    <a
+      class="carousel-control-next"
+      href="#carouselExampleSlidesOnly"
+      role="button"
+      data-slide="next"
+    >
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
   </div>
-     
-    
-     <br>
-    <!--première partie -->
+
+  <br />
+  <!--première partie -->
   <div class="container-fluid">
     <div class="row no-gutters">
       <div class="col-12 col-md-4">
@@ -72,86 +81,91 @@
     </div>
   </div>
 
+  <br />
+  <br />
+  <!-- New Product Category Section with Carousel -->
+  <h2 class="text-center"><strong>Nouvelles catégories</strong></h2>
 
-     <br>
-     <br>
-    <!-- New Product Category Section with Carousel -->
-    <h2 class="text-center"><strong>Nouvelles catégories</strong></h2>
-    
-    <div id="product-category-section" class="container mt-5">
-      <div id="productCarousel" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div v-for="(chunk, chunkIndex) in categoryChunks" :key="chunkIndex" :class="['carousel-item', { active: chunkIndex === 0 }]">
-            <div class="row justify-content-center">
-              <ProductCategoryCard
-                v-for="(category, index) in chunk"
-                :key="index"
-                :imageSrc="category.imageSrc"
-                :categoryTitle="category.categoryTitle"
-                :productCount="category.productCount"
-                :linkUrl="category.linkUrl"
-
-              />
-            </div>
+  <div id="product-category-section" class="container mt-5">
+    <div id="productCarousel" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div
+          v-for="(chunk, chunkIndex) in categoryChunks"
+          :key="chunkIndex"
+          :class="['carousel-item', { active: chunkIndex === 0 }]"
+        >
+          <div class="row justify-content-center">
+            <ProductCategoryCard
+              v-for="(category, index) in chunk"
+              :key="index"
+              :imageSrc="category.imageSrc"
+              :categoryTitle="category.categoryTitle"
+              :productCount="category.productCount"
+              :linkUrl="category.linkUrl"
+            />
           </div>
         </div>
-        <a class="carousel-control-prev" href="#productCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon  custom-carousel-control-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next  custom-carousel-control-icon" href="#productCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+      </div>
+      <a class="carousel-control-prev" href="#productCarousel" role="button" data-slide="prev">
+        <span
+          class="carousel-control-prev-icon custom-carousel-control-icon"
+          aria-hidden="true"
+        ></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next custom-carousel-control-icon"
+        href="#productCarousel"
+        role="button"
+        data-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+  <br />
+  <br />
+  <br />
+  <br />
+  <!-- New Arrivals Section -->
+  <div id="new-arrivals-section" class="container mt-5">
+    <h2 class="text-center"><strong>Nouvelles arrivées</strong></h2>
+    <div class="row">
+      <ProductCard
+        v-for="(product, index) in products"
+        :key="index"
+        :imageSrc="product.imageSrc"
+        :productTitle="product.productTitle"
+        :productPrice="product.productPrice"
+        :productRating="product.productRating"
+        :productReviews="product.productReviews"
+      />
+    </div>
+
+    <div class="text-center mt-4">
+      <a href="#" class="btn btn-link" @click="loadMoreProducts" v-if="!isLoading">Voir plus</a>
+    </div>
+    <div v-if="isLoading" class="text-center mt-4">
+      <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
       </div>
     </div>
-    <br>
-     <br>
-     <br>
-     <br>
-    <!-- New Arrivals Section -->
-    <div id="new-arrivals-section" class="container mt-5">
-      <h2 class="text-center"><strong>Nouvelles arrivées</strong></h2>
-      <div class="row">
-        <ProductCard
-          v-for="(product, index) in products"
-          :key="index"
-          :imageSrc="product.imageSrc"
-          :productTitle="product.productTitle"
-          :productPrice="product.productPrice"
-          :productRating="product.productRating"
-          :productReviews="product.productReviews"
-          
-        />
-      </div>
-      
-      <div class="text-center mt-4">
-        <a href="#" class="btn btn-link" @click="loadMoreProducts" v-if="!isLoading">Voir plus</a>
-      </div>
-      <div v-if="isLoading" class="text-center mt-4">
-        <div class="spinner-border" role="status">
-          <span class="sr-only">Loading...</span>
-        </div>
-      </div>
-   
   </div>
 </template>
 
 <script>
-import ProductCard from '/src/components/component1.vue';
-import ProductCategoryCard from '/src/components/component2.vue';
-import ProductCard2 from '/src/components/component3.vue'
-import CarouselItem from '/src/components/component4.vue'
-
+import ProductCard from '@/components/ProductCard.vue'
+import ProductCategoryCard from '@/components/ProductCategoryCard.vue'
+import ProductCard2 from '@/components/ProductCard2.vue'
+import CarouselItem from '@/components/CarouselItem.vue'
 
 export default {
   components: {
     ProductCard,
     ProductCategoryCard,
     ProductCard2,
-    CarouselItem,
-
-
+    CarouselItem
   },
   data() {
     return {
@@ -161,100 +175,92 @@ export default {
           productTitle: 'Sonos Beam Gen 2 Soundbar',
           productPrice: '$930.00',
           productRating: 3,
-          productReviews: 9,
+          productReviews: 9
         },
         {
           imageSrc: '/src/assets/SAC.png',
           productTitle: 'Bose Smart Soundbar 900',
           productPrice: '$689.00',
           productRating: 3,
-          productReviews: 9,
+          productReviews: 9
         },
         {
           imageSrc: '/src/assets/SAC.png',
           productTitle: 'Sennheiser Ambeo Soundbar',
           productPrice: '$1,679.00',
           productRating: 4,
-          productReviews: 10,
+          productReviews: 10
         },
         {
           imageSrc: '/src/assets/SAC.png',
           productTitle: 'Microsoft Surface Pro 8',
           productPrice: '$606.00',
           productRating: 4,
-          productReviews: 10,
-        },
+          productReviews: 10
+        }
       ],
       categories: [
         {
           imageSrc: '/src/assets/A.png',
           categoryTitle: 'Headphones',
           productCount: 5,
-          linkUrl: '/headphones',
-
+          linkUrl: '/headphones'
         },
         {
           imageSrc: '/src/assets/A.png',
           categoryTitle: 'With Bluetooth',
           productCount: 7,
-          linkUrl: '/soundbars',
-
+          linkUrl: '/soundbars'
         },
         {
           imageSrc: '/src/assets/A.png',
           categoryTitle: 'Mobile Phone',
           productCount: 5,
-          linkUrl: '/soundbars',
-
+          linkUrl: '/soundbars'
         },
         {
           imageSrc: '/src/assets/A.png',
           categoryTitle: 'CPU Heat Pipes',
           productCount: 7,
-          linkUrl: '/soundbars',
-
+          linkUrl: '/soundbars'
         },
         {
           imageSrc: '/src/assets/A.png',
           categoryTitle: 'Smart Watch',
           productCount: 6,
-          linkUrl: '/soundbars',
-
+          linkUrl: '/soundbars'
         },
         {
           imageSrc: '/src/assets/A.png',
           categoryTitle: 'Smart Watch',
           productCount: 6,
-          linkUrl: '/soundbars',
-
+          linkUrl: '/soundbars'
         },
         {
           imageSrc: '/src/assets/A.png',
           categoryTitle: 'Smart Watch',
           productCount: 6,
-          linkUrl: '/soundbars',
-
+          linkUrl: '/soundbars'
         },
         {
           imageSrc: '/src/assets/A.png',
           categoryTitle: 'Smart Watch',
           productCount: 6,
-          linkUrl: '/soundbars',
-
-        },
+          linkUrl: '/soundbars'
+        }
       ],
-      isLoading: false,
-    };
+      isLoading: false
+    }
   },
   computed: {
     categoryChunks() {
-      return this.chunkArray(this.categories, 5);
-    },
+      return this.chunkArray(this.categories, 5)
+    }
   },
   methods: {
     loadMoreProducts(event) {
-      event.preventDefault();
-      this.isLoading = true;
+      event.preventDefault()
+      this.isLoading = true
       // Simuler un chargement asynchrone
       setTimeout(() => {
         const moreProducts = [
@@ -263,36 +269,36 @@ export default {
             productTitle: 'Apple AirPods Pro',
             productPrice: '$249.00',
             productRating: 5,
-            productReviews: 120,
+            productReviews: 120
           },
           {
             imageSrc: '/src/assets/SAC.png',
             productTitle: 'Samsung Galaxy Buds',
             productPrice: '$129.00',
             productRating: 4,
-            productReviews: 95,
+            productReviews: 95
           },
           {
             imageSrc: '/src/assets/SAC.png',
             productTitle: 'Sony WH-1000XM4',
             productPrice: '$349.00',
             productRating: 5,
-            productReviews: 220,
-          },
-        ];
-        this.products.push(...moreProducts);
-        this.isLoading = false;
-      }, 2000);
+            productReviews: 220
+          }
+        ]
+        this.products.push(...moreProducts)
+        this.isLoading = false
+      }, 2000)
     },
     chunkArray(arr, size) {
-      const result = [];
+      const result = []
       for (let i = 0; i < arr.length; i += size) {
-        result.push(arr.slice(i, i + size));
+        result.push(arr.slice(i, i + size))
       }
-      return result;
-    },
-  },
-};
+      return result
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -325,12 +331,11 @@ export default {
 }
 
 .custom-carousel-img {
-  max-height: 500px; 
+  max-height: 500px;
   object-fit: cover;
 }
 /* New CSS for Carousel Controls */
 .custom-carousel-control-icon {
-  
   filter: invert(1); /* Change the color of the icon to black */
 }
 .container-fluid {
