d2922cec5eb0ec63efcee00083df452472c939fa
Blaise-dev <blaiseatangana5@gmail.com>
2024-06-12 16:33:14 +0200

    Modif Commentaire

--- src/views/ProductDetailsView.vue
+++ src/views/ProductDetailsView.vue
@@ -342,7 +342,7 @@ export default {
       }
       this.comments.push(newComment)
       this.newComment = {
-        name: 'Théo Backbwell',
+        author: 'Théo Backbwell',
         text: '',
         rating: 0
       }
